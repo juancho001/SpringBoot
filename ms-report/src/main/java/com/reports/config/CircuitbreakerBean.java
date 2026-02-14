ad9f6430f75f283af6e26829d7c3bd4c889824dc
HP5000SERIES\avila <avilameza@gmail.com>
2026-02-14 11:17:38 -0500

    ajuste del ms-report  para que trabajar con el circuitbreaker internamente en este componente

---
+++ ms-report/src/main/java/com/reports/config/CircuitbreakerBean.java
@@ -0,0 +1,38 @@
+package com.reports.config;
+
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class CircuitbreakerBean {
+
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> globalCustomCB(){
+        var circuitBreakerConfig = CircuitBreakerConfig
+                .custom()
+                .failureRateThreshold(60)
+                .waitDurationInOpenState(Duration.ofSeconds(90))
+                .slidingWindowSize(10)
+                .build();
+
+        var timeLimiterConfig = TimeLimiterConfig
+                .custom()
+                .timeoutDuration(Duration.ofSeconds(5))
+                .build();
+
+        return factory -> factory.configureDefault(
+                id -> new Resilience4JConfigBuilder(id)
+                        .timeLimiterConfig(timeLimiterConfig)
+                        .circuitBreakerConfig(circuitBreakerConfig)
+                        .build()
+        );
+    }
+}
